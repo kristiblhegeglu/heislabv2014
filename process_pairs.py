4f39f4cf69d2a0e6c72311e0482a5be357a1d979
kristiblhegeglu <kristibl@stud.ntnu.no>
2014-02-14 09:47:30 +0100

    Update process_pairs.py

--- process_pairs.py
+++ process_pairs.py
@@ -3,26 +3,33 @@
 import os
 import socket
 import time
-
-def counter():
-	i = 0
-	for i in range(5):
-		print i+1
-		time.sleep(1)
+import select
 
 
 UDP_PORT = 30015
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('', 0))
+s.setblocking(0)
+s.bind(('0.0.0.0', UDP_PORT))
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-while 1:
-    data = repr(time.time()) + '\n'
-    #try:
-    #s.sendto(data, (counter(), UDP_PORT))
-    s.sendto(data, ('Hei',UDP_PORT))
-    #except:
-    	#print 'Failed to send message'
-    time.sleep(2)
+i=0
+
+while True:
+	print "Waiting for data"
+	ready = select.select([s],[],[], 3)
+	if ready[0]:
+		data, address = s.recvfrom(1024)
+		i = int(data.split()[1])		
+		print "Got data:", data
+	else:
+		print "Other process died"
+		break
+
+while True:
+	i += 1
+	data = "Count: "+str(i)
+	print data
+	s.sendto(data, ("129.241.187.255",UDP_PORT))
+	time.sleep(2)
 
