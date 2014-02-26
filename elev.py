5af5b8c726645c33d8200c3f17b1831ea03c5b60
kristiblhegeglu <kristibl@stud.ntnu.no>
2014-02-26 13:22:41 +0100

    Delete elev.py

--- elev.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class elev_button_type_t(Enum):
-	_order_ = 'BUTTON_CALL_UP BUTTON_CALL_DOWN BUTTON_COMMAND'
-	BUTTON_CALL_UP = 0
-	BUTTON_CALL_DOWN = 1
-	BUTTON_COMMAND = 2 
-
