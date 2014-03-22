094642f1322000e71b6b392953565b31a6d1d78e
kristiblhegeglu <kristibl@stud.ntnu.no>
2014-03-22 08:59:09 +0100

    added comments

--- driver.py
+++ driver.py
@@ -3,5 +3,7 @@ import ctypes
 ctypes.cdll.LoadLibrary("./elev.so")
 elev = ctypes.CDLL("./elev.so")
 
-
+#here we have "converted" all the c files from the driver we got handed out,
+#so we physically didn't need to write them from c to python.
+#the c files can be called by writing elev.elev_..... (the name you are looking for)
 
